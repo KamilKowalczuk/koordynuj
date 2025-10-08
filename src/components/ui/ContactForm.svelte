539aeeeeee94e0dbde2f622f36097215ec7aa5ac
Kamil Kowalczuk <kamil.kowalczuk17@gmail.com>
2025-10-08 15:19:12 +0200

    porpawa kontakt

--- src/components/ui/ContactForm.svelte
+++ src/components/ui/ContactForm.svelte
@@ -41,7 +41,7 @@
         errorMsg = null;
 
         try {
-            const res = await fetch('/api/contact', {
+            const res = await fetch('/api/submit-form', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({
